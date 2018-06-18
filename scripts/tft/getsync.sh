1081d8725f147c2d2fcfb4b3fd15fa4995131a7f
kristof@jimber.org <kristof@jimber.org>
2018-06-18 14:51:27 +0200

    bug in getsync tft

--- scripts/tft/getsync.sh
+++ scripts/tft/getsync.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-tfchainc consensus | grep Progress | cut -d' ' -f3 | cut -d'%' -f1
+SYNCED=$(tfchainc consensus | grep Synced)
+
+if [[ -z "$SYNCED" ]]; then
+  tfchainc consensus | grep Progress | cut -d' ' -f3 | cut -d'%' -f1
+else
+  echo 100
+fi
